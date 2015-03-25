81e6804107e12f98fd308eb7da750139415b1631
tapnair <patrick.rainsberry@gmail.com>
2015-03-24 20:36:05 -0700

    Added implementations

--- octoprint_octodisplay/__init__.py
+++ octoprint_octodisplay/__init__.py
@@ -6,3 +6,5 @@ import octoprint.plugin
 class OctoDisplayPlugin(octoprint.plugin.StartupPlugin):
     def on_after_startup(self):
         self._logger.info("Hello World! - OctoDisplay")
+
+__plugin_implementations__ = [OctoDisplayPlugin()]
