b5d7cf5803fe452d29f1e6136cd606f382507397
tapnair <prrainsberry@yahoo.com>
2015-03-24 21:03:59 -0700

    committed from pycharm

--- octoprint_octodisplay/__init__.py
+++ octoprint_octodisplay/__init__.py
@@ -3,8 +3,19 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 
-class OctoDisplayPlugin(octoprint.plugin.StartupPlugin):
+class OctoDisplayPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.EventHandlerPlugin):
     def on_after_startup(self):
         self._logger.info("Hello World! - OctoDisplay")
+
+    def on_event(self, event, payload):
+        #if event == "PrintStarted":
+        if event == "Connected":
+            self._logger.info("OctoDisplay - Printer Connected")
+            self._logger.info(payload['port'])
+
+
+
+
+
 
 __plugin_implementations__ = [OctoDisplayPlugin()]
