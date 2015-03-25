6b449d8eadf5bc88ca2bf727e27e5240353d7621
tapnair <patrick.rainsberry@gmail.com>
2015-03-24 20:05:44 -0700

    Update setup.py
    testing update edited description

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ plugin_version = "0.1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = "Display status on a LCD Dispaly"
+plugin_description = "Display printer status on a LCD Dispaly"
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Patrick Rainsberry"
